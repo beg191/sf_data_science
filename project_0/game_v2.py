bde6cefd105cb90323610e0e19a08c21b73b3f28
beg191 <begunov@mail.ru>
2022-02-19 11:37:25 +0800

    edit algoritm random_predict

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -10,14 +10,20 @@ def random_predict(number:int=1)->int:
         int: Число попыток
     """
     count = 0
-    
+    count_min = -1
+    count_max = 101
     while True:
         count += 1
-        predict_number = np.random.randint(1,101) #Предполагаемое число
+        predict_number = (count_max - count_min)//2 + count_min #Предполагаемое число
+        if number > predict_number:
+            count_min = predict_number
+        if number < predict_number:
+            count_max = predict_number
+#       predict_number = np.random.randint(1,101) #Предполагаемое число
         if number == predict_number:
             break #выход из цикла, если угадали
     return(count)
-    
+   
     
 def score_game(random_predict)->int:
     """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
